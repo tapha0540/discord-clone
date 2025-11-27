5d79c58733a1faf44d72987e4b087461f860df58
Tapha0540 <fmoustapha095@gmail.com>
2025-11-27 18:25:21 +0000

    I fix the issues with tabsbar navigations

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,76 @@
 import { Tabs } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  FontAwesome,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
 
-export default function TabLayout() {
+const TabLayout = () => {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        headerTitleStyle: { fontSize: 18, color: "black", fontWeight: "bold" },
+        tabBarInactiveTintColor: "grey",
+        tabBarActiveTintColor: "black",
       }}
     >
-      <Tabs.Screen name="index" options={{ title: "Accueil" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Accueil",
+          tabBarIcon: ({ color, size }) => {
+            return <MaterialIcons name="home-work" size={size} color={color} />;
+          },
+        }}
+      />
       <Tabs.Screen
         name="notifications"
         options={{
           title: "Notifications",
           headerShown: true,
           headerTitle: "Notifications",
-          tabBarIcon: ({
-            focused,
-            color,
-            size,
-          }: {
-            focused: boolean;
-            color: string;
-            size: number;
-          }) => {
+          headerRight: ({ tintColor }) => {
             return (
-              <View>
-                <Text>ok</Text>
+              <View style={styles.headerRight}>
+                <SimpleLineIcons name="options" size={20} color={tintColor} />
               </View>
+            );
+          },
+          tabBarIcon: ({ color, size }) => {
+            return <Octicons name="bell-fill" size={size} color={color} />;
+          },
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Toi",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <FontAwesome name="user-circle-o" size={size} color={color} />
             );
           },
         }}
       />
-      <Tabs.Screen name="profile" options={{ title: "Toi" }} />
     </Tabs>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  headerRight: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 40,
+    height: 40,
+    backgroundColor: "#ededed",
+    marginRight: 10,
+    borderRadius: 20,
+  },
+});
+
+export default TabLayout;
